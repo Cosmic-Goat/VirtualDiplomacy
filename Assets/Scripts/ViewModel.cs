7ab546a5139dd0c35d3bd41185adec7d79c797af
Shrey Pasricha <shrey.pasricha@gmail.com>
2018-05-26 15:41:31 -0400

    Added Animations

--- Assets/Scripts/ViewModel.cs
+++ Assets/Scripts/ViewModel.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class X : MonoBehaviour {
+public class ViewModel : MonoBehaviour {
 
+    public Animation textAnimation;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Animate()
+    {
+
+    }
 }
