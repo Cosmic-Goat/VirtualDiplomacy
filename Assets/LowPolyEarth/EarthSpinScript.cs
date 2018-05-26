f6d0d202badf85e02db7c62755184c0047c400d2
MatthewChen37 <epicninjasauce@gmail.com>
2018-05-26 15:19:52 -0400

    LOTS OF STUFF

--- Assets/LowPolyEarth/EarthSpinScript.cs
+++ Assets/LowPolyEarth/EarthSpinScript.cs
@@ -4,7 +4,17 @@ using System.Collections;
 public class EarthSpinScript : MonoBehaviour {
     public float speed = 10f;
 
+    public bool whenTouched = false;
+
+
+
+
     void Update() {
         transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        if(whenTouched == true)
+        {
+            transform.Rotate(0, 0, 0);
+
+        }
     }
 }
---
+++ Assets/TextScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class TextScript : MonoBehaviour {
+
+    public Slider slider;
+    public Text yearsss;
+    string[] data = new string[10];
+    
+    List<string> years = new List<string>();
+    // Use this for initialization
+    void Start () {
+       slider.onValueChanged.AddListener(delegate { TextUpdate(); });
+
+        yearsss = GetComponent<Text>();
+        for(int i = 0; i < data.Length; i++)
+        {
+            years.Add("Year: " + data[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	public void TextUpdate ()
+    {
+        Debug.Log((int)slider.value);
+        yearsss.text = years[(int)slider.value];
+    }
+}
